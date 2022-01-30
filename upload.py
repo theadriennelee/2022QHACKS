3411f29a77df41ea204d72a9d764122a58204a54
RQX <35436350+xzx6vii007@users.noreply.github.com>
2022-01-30 00:57:39 -0500

    Got rid of modal
    Modal bug caused file to not transfer

--- upload.py
+++ upload.py
@@ -3,26 +3,30 @@
 from flask import Flask, render_template, request, send_from_directory
 # from werkzeug import secure_filename
 from werkzeug.utils import secure_filename
-from werkzeug.datastructures import  FileStorage
+from werkzeug.datastructures import FileStorage
 import os
 from transcriber import transcriber
 from pdfmake import pdf
 import shutil
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     # return("Hello World")
     return render_template('homepage.html')
 
+
 @app.route('/theNotebook')
 def theNotebook():
     return render_template('theNotebook.html')
 
+
 # @app.route('/upload')
 # def upload_file():
 #    return render_template('transcription.html')
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
     # return render_template('transcription.html')
     # app.logger("hello")
@@ -31,14 +35,14 @@ def upload_file():
             f = request.files['myFile']
             f.save(secure_filename(f.filename))
             shutil.copyfile("C:\\Users\\Admin\\Documents\\GitHub\\2022QHACKS\\" + f.filename,
-                            "C:\\Users\\Admin\\Documents\\GitHub\\2022QHACKS\\clone1.mp4" )
+                            "C:\\Users\\Admin\\Documents\\GitHub\\2022QHACKS\\clone1.mp4")
             test = transcriber(f.filename)
             test.wait()
             foo = test.getparagraphs()
             start, end = test.gettstamps()
             print("C:\\Users\\Admin\\Documents\\GitHub\\2022QHACKS\\" + f.filename)
-            #app.logger(bar)
-            pdf1 = pdf(f.filename, foo,start,end, "C:\\Users\\Admin\\Documents\\GitHub\\2022QHACKS\\clone1.mp4")
+            # app.logger(bar)
+            pdf1 = pdf(f.filename, foo, start, end, "C:\\Users\\Admin\\Documents\\GitHub\\2022QHACKS\\clone1.mp4")
             pdf1.gen2()
             #   return 'file uploaded successfully'
             return render_template("transcription.html")
@@ -46,15 +50,17 @@ def upload_file():
         error_message = "Please select a file."
         return render_template("theNotebook.html", message=error_message)
 
+
 @app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
 def download(filename):
     # Appending app path to upload folder path within app root folder
     # uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
     foobar = os.getcwd()
-    #uploads = r"C:\Users\Adrienne Lee\Documents\Queens\Year 4\2022QHACKS"
+    # uploads = r"C:\Users\Adrienne Lee\Documents\Queens\Year 4\2022QHACKS"
     # Returning file from appended path
     # return send_from_directory(directory=uploads, filename=filename)
     return send_from_directory(foobar, filename)
-		
+
+
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
